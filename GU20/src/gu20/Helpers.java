a87b9c105a5e5d9c65b224e9424d6debbc54fd25
Pontus Laos <laos.pontus@gmail.com>
2019-03-05 09:48:38 +0100

    Added a helper method for getting a non-loopback IPv4 address.

--- GU20/src/gu20/Helpers.java
+++ GU20/src/gu20/Helpers.java
@@ -1,6 +1,11 @@
 package gu20;
 
 import java.io.IOException;
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.Enumeration;
 import java.util.logging.FileHandler;
 import java.util.logging.Logger;
 import java.util.logging.SimpleFormatter;
@@ -29,5 +34,33 @@ public final class Helpers {
 	    } 
 	    catch (SecurityException e) {} 
 	    catch (IOException e) {}
+	}
+	
+	/**
+	 * Mostly for Linux: gets the first external IPv4 address of the computer.
+	 * @return An IPv4 address that other computers can access, null if none is found or if the network interfaces can not be accessed.
+	 */
+	public static InetAddress getFirstNonLoopbackIPv4Address() {
+	    Enumeration<NetworkInterface> networkInterfaces = null;
+
+	    try { 
+	    	networkInterfaces = NetworkInterface.getNetworkInterfaces(); 
+    	}
+	    catch (SocketException ex) { 
+	    	return null; 
+    	}
+
+	    while (networkInterfaces.hasMoreElements()) {
+	        NetworkInterface i = (NetworkInterface) networkInterfaces.nextElement();
+	        for (Enumeration<InetAddress> en2 = i.getInetAddresses(); en2.hasMoreElements();) {
+	            InetAddress address = (InetAddress) en2.nextElement();
+	            
+	            if (!address.isLoopbackAddress() && address instanceof Inet4Address) {
+	            	return address;
+	            }
+	        }
+	    }
+	    
+	    return null;
 	}
 }
