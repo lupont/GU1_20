077c59109fdb46079429bbda59d9a92d2916ee62
Pontus Laos <laos.pontus@gmail.com>
2019-03-13 14:31:06 +0100

    Fixed a bug where ip was null when logging onto a remote server.

--- GU20/src/gu20/GUIController.java
+++ GU20/src/gu20/GUIController.java
@@ -246,6 +246,8 @@ public class GUIController {
 			avatar = null;
 		}
 		
+		addresses.put(host, host);
+		
 		user = new MockUser(username, avatar);
 		client = new MockClient(user, addresses.get(host), port);
 		client.setGUIController(this);
